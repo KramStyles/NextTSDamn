4b424ee99f110ee1cbe7148ad664f0f6de46e425
kramstyles <kramstyles@outlook.com>
2024-08-25 15:19:07 -0700

    added server side rendering and loading.tsx

--- next-ts-tutorial/app/layout.tsx
+++ next-ts-tutorial/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={inter.className}>
-        <p>This is the root layoutu</p>
+        <p>This is the root layout</p>
             {children}
         </body>
         </html>
