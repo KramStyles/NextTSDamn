0d35361db330c42e26016c9cf972f25004925301
kramstyles <kramstyles@outlook.com>
2024-08-25 09:38:11 -0700

    added grouped routes

---
+++ next-ts-tutorial/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+const Dashboard = () => {
+    return <>
+        <h1>Dashboard Page from Grouping.</h1>
+        <code>/dashboard</code>
+    </>;
+};
+
+export default Dashboard;
---
+++ next-ts-tutorial/app/admin/dashboard/page.tsx
@@ -0,0 +1,8 @@
+const Dashboard = () => {
+  return <>
+    <h1>Dashboard without Grouping</h1>
+      <p>/admin/dashboard</p>
+  </>
+}
+
+export default Dashboard;
