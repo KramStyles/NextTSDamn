ec5468509285636353a2f06080ba7a208bd1f456
kramstyles <kramstyles@outlook.com>
2024-08-27 09:00:35 -0700

    added header section

--- next-ts-tutorial/app/page.tsx
+++ next-ts-tutorial/app/page.tsx
@@ -1,10 +1,9 @@
-import ClientButton from "@/app/ClientButton";
+
 
 export default function HomeComponent() {
     return (
         <div>
             <h1>Hello there, I am a server component.</h1>
-            <ClientButton />
         </div>
     )
 }
