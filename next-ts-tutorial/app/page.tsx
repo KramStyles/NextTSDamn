488cf5464a0011a3bc15fb3652cd902a1462eb6e
kramstyles <kramstyles@outlook.com>
2024-09-01 11:31:05 -0700

    fetched and added error page

--- next-ts-tutorial/app/page.tsx
+++ next-ts-tutorial/app/page.tsx
@@ -1,5 +1,6 @@
 // Get constants from environmental variables.
 import React from "react";
+import Results, {ResultProps} from "@/components/Results";
 
 const API_KEY = process.env["REACT_APP_API_KEY"]
 const API_URL = process.env["REACT_APP_MOVIE_API"]
@@ -13,18 +14,23 @@ export default async function HomeComponent ({ searchParams }: { searchParams: a
     const param = searchParams.genre || "trending";
     const genre = `${param === "trending" ? "/trending/all/week" : "/movie/top_rated"}`
     const url = `${API_URL}${genre}?api_key=${API_KEY}&language=en-US&page=1`;
+    console.log(url);
     const response = await fetch(url, {
         next: {
             revalidate: 86400, // Caches and removes cache after 1 day.
         }
     })
     const data = await response.json();
-    if(!response.ok) throw new Error("Failed to fetch data.")
+    const msg = `Failed to fetch data. ${response.statusText }`
+    if(!response.ok) throw new Error(msg)
 
     const results = data.results;
     return (
         <div>
             <h1>Hello there, I am a server component.</h1>
+            {results.map((result: ResultProps) => (
+               <Results result={result} key={result.id}/>
+            ))}
         </div>
     )
 }
