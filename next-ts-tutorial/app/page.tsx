485afe5792aa4592eedb01bbff1c58a8c3a961c7
kramstyles <kramstyles@outlook.com>
2024-09-03 14:56:46 -0700

    added image using the Image tag

--- next-ts-tutorial/app/page.tsx
+++ next-ts-tutorial/app/page.tsx
@@ -41,8 +41,7 @@ export default async function HomeComponent({
 
   const results = data.results;
   return (
-    <div>
-      <h1>Hello there, I am a server component.</h1>
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
       {results.map((result: ResultProps) => (
         <Results result={result} key={result.id} />
       ))}
