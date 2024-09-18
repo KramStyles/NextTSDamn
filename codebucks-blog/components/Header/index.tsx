057bb76c8aefb79cf4f1418273874a0fee78ac58
kramstyles <kramstyles@outlook.com>
2024-09-18 11:19:27 -0700

    added icons

--- codebucks-blog/components/Header/index.tsx
+++ codebucks-blog/components/Header/index.tsx
@@ -5,23 +5,51 @@
 
 import Logo from "@/components/Header/Logo";
 import Link from "next/link";
+import {
+    LinkedinIcon,
+    GithubIcon,
+    TwitterIcon,
+    DribbbleIcon, SunIcon,
+} from "@/components/Icons";
 
 const Header = () => {
   return (
     <header className="w-full py-4 px-10 flex items-center justify-between">
       <Logo />
-      <nav className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex
-      items-center gap-x-3 fixed top-6 bg-light/50 backdrop-blur-sm right-1/2 translate-x-1/2">
+      <nav
+        className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex
+      items-center gap-x-3 fixed top-6 bg-light/50 backdrop-blur-sm right-1/2 translate-x-1/2"
+      >
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/contact">Contact</Link>
-        <button>T</button>
+        <button><SunIcon /></button>
       </nav>
       <div>
-          <a href="https://example.com/kramstyles">LinkedIn</a>
-          <a href="https://example.com/kramstyles">LinkedIn</a>
-          <a href="https://example.com/kramstyles">LinkedIn</a>
-          <a href="https://example.com/kramstyles">LinkedIn</a>
+        <a
+          href="https://github.com/kramstyles"
+          className="inline-block w-6 h-6 mx-2"
+        >
+          <GithubIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a
+          href="https://twitter.com/kramstyles"
+          className="inline-block w-6 h-6 mx-2"
+        >
+          <TwitterIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a
+          href="https://linkedin.com/in/eke-mark"
+          className="inline-block w-6 h-6 mx-2"
+        >
+          <LinkedinIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a
+          href="https://example.com/kramstyles"
+          className="inline-block w-6 h-6 mx-2"
+        >
+          <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
       </div>
     </header>
   );
