0e7f9d27796062f09c878709b02c94de973c17ec
kramstyles <kramstyles@outlook.com>
2024-09-19 05:32:30 -0700

    added scss

--- cms_blog/src/app/page.js
+++ cms_blog/src/app/page.js
@@ -1,10 +1,8 @@
+import { PostCard, Categories, PostWidget } from "@/components";
+
 export default function Home() {
   const posts = [
     { title: "React Testing", excerpt: "Learn React Testing" },
-    {
-      title: "React with Tailwind",
-      excerpt: "Learn React Testing with Tailwind",
-    },
     {
       title: "React with Tailwind",
       excerpt: "Learn React Testing with Tailwind",
@@ -15,13 +13,14 @@ export default function Home() {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8 ">
           {posts.map((post, index) => (
-            <div key={index}>
-              {post.title} <p>{post.excerpt}</p>
-            </div>
+            <PostCard post={post} key={index} />
           ))}
         </div>
         <div className="col-span-1 lg:col-span-4">
-          <div className="lg:sticky relative top-8"></div>
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
     </div>
