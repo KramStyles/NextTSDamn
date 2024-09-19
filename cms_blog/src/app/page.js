723460b32f50dd8c08a241a235a6f2c7654ab3f6
kramstyles <kramstyles@outlook.com>
2024-09-19 04:57:23 -0700

    created new JS blog..

---
+++ cms_blog/src/app/page.js
@@ -0,0 +1,29 @@
+export default function Home() {
+  const posts = [
+    { title: "React Testing", excerpt: "Learn React Testing" },
+    {
+      title: "React with Tailwind",
+      excerpt: "Learn React Testing with Tailwind",
+    },
+    {
+      title: "React with Tailwind",
+      excerpt: "Learn React Testing with Tailwind",
+    },
+  ];
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="col-span-1 lg:col-span-8 ">
+          {posts.map((post, index) => (
+            <div key={index}>
+              {post.title} <p>{post.excerpt}</p>
+            </div>
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="lg:sticky relative top-8"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
